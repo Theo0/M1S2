63e8fb20975302734204b6fe15855cac2825d396
Theo0 <theo341@gmail.com>
2015-02-10 15:18:34 +0100

    visual studio tp

---
+++ ArchiNTiers/TP2/exemplesCoursRemoting/ExemplePassageRefClient/Client.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+
+namespace RemotingSamples
+{
+    public class Client
+    {
+        public static int Main(string[] args)
+    {
+        TcpChannel chan = new TcpChannel(8086);
+      ChannelServices.RegisterChannel(chan, true);
+      ForwardMe param = new ForwardMe();
+      HelloServer obj = (HelloServer)Activator.GetObject(typeof(RemotingSamples.HelloServer), "tcp://localhost:8085/SayHello");
+      if (obj == null) System.Console.WriteLine("Impossible de trouver le serveur");
+      else Console.WriteLine(obj.HelloMethod("Homme des cavernes",param));
+      Console.Read();
+      return 0;
+    }
+    }
+}
---
+++ ArchiNTiers/TP2/exemplesCoursRemoting/ExempleSingleton/Serveur.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels.Http;
+
+namespace RemotingSamples {
+  public class Sample {
+      public static int Main(string [] args) {
+      TcpChannel chan1 = new TcpChannel(8089); 
+      ChannelServices.RegisterChannel(chan1, true);
+      RemotingConfiguration.RegisterWellKnownServiceType(typeof(HelloServer), "SayHello", WellKnownObjectMode.Singleton);
+      System.Console.WriteLine("Appuyez sur <entree> pour sortir...");
+      System.Console.ReadLine();
+      return 0;
+    }
+  }
+}
